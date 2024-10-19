bbcfc08f36d495a38869894517777c5ca1321b6d
Henrique Manoel Vieira <76623526+ziiklinux@users.noreply.github.com>
2024-10-19 19:30:19 -0300

    main.tf
    Arquivo main.tf com as melhorias de segurança e a automação solicitada no desafio.

---
+++ main.tf
@@ -0,0 +1,222 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "projeto" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "VExpenses"
+}
+
+variable "candidato" {
+  description = "Nome do candidato"
+  type        = string
+  default     = "Henrique Manoel Vieira"
+}
+
+variable "ips_ssh" {
+  description = "Lista de IPs permitidos para acesso SSH"
+  type        = list(string)
+  default     = ["192.168.1.1/32", "10.0.0.1/32"]  # Substitua pelos IPs permitidos
+}
+
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "${var.projeto}-${var.candidato}${var.ips_ssh}-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-vpc"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-igw"
+  }
+}
+
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-route_table"
+  }
+}
+
+resource "aws_route_table_association" "main_association" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-route_table_association"
+  }
+}
+
+resource "aws_security_group" "main_sg" {
+  name        = "${var.projeto}-${var.candidato}${var.ips_ssh}-sg"
+  description = "Permitir SSH de IPs específicos e tráfego de entrada e saída limitados"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  # Regras de entrada
+  ingress {
+    description = "Permite entrada SSH de IPs específicos"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ips_ssh
+    ipv6_cidr_blocks = var.ips_ssh
+  }
+
+  ingress {
+    description = "Permite entrada HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }  
+  ingress {
+  description = "Permite entrada HTTP para o uso nginx"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+
+  }
+  
+  ingress {
+    description = "Permite entrada DNS"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  # Regras de saída
+  egress {
+    description = "Permite saída SSH de IPs específicos"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ips_ssh
+    ipv6_cidr_blocks = var.ips_ssh
+   }
+    
+  egress {
+    description = "Permite saída HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description = "Permite saída HTTP para o uso nginx"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description = "Permite saída DNS"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-sg"
+  }
+}
+
+data "aws_ami" "debian12" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-12-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"]
+}
+
+resource "aws_instance" "debian_ec2" {
+  ami             = data.aws_ami.debian12.id
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.main_subnet.id
+  key_name        = aws_key_pair.ec2_key_pair.key_name
+  security_groups = [aws_security_group.main_sg.name]
+
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get upgrade -y
+              apt-get install -y nginx # Instala o nginx
+              systemctl start nginx # Inicia o nginx 
+              systemctl enable nginx # Habilita o nginx a iniciar automaticamente
+              EOF
+
+  tags = {
+    Name = "${var.projeto}-${var.candidato}${var.ips_ssh}-ec2"
+  }
+}
+
+output "private_key" {
+  description = "Chave privada para acessar a instância EC2"
+  value       = tls_private_key.ec2_key.private_key_pem
+  sensitive   = true
+}
+
+output "ec2_public_ip" {
+  description = "Endereço IP público da instância EC2"
+  value       = aws_instance.debian_ec2.public_ip
+}
